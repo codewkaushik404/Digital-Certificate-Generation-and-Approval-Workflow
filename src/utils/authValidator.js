f1fe9e78847660baa21a56cb319d5e3d3fff7f49
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-02-06 13:00:28 +0530

    feat(validation): add custom Zod validation for user input from login and register API'

---
+++ src/utils/authValidator.js
@@ -0,0 +1,26 @@
+const zod = require("zod");
+
+const roleEnum = zod.enum(["ADMIN", "FACULTY", "HOD", "TEAM-LEAD", "CORE-TEAM", "PRESIDENT", "CLUB-COORDINATOR" ]);
+const departmentEnum = zod.enum(["CSE", "AIML", "ISE", "CIVIL", "ECE", "EEE", "MECH"], 
+    {message: "Invalid department name"});
+
+const loginValidate = zod.object({
+    email: zod.string().regex(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/),
+    password: zod.string({message: "Password must be a string"}).min(8, {message: "Password must have atleast 8 characters"})
+});
+
+//name, email, password, instituteName, department, joiningYear, role
+const registerValidate = zod.object({
+    name: zod.string(),
+    email: zod.string().regex(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/),
+    password: zod.string().min(8),
+    instituteName: zod.string().min(5,{message: "Institute Name must be atleast 5 characters"}),
+    department: departmentEnum,
+    duration: zod.number().optional().default(4),
+    joiningYear: zod.number().min(2000).max(new Date().getFullYear()),
+});
+
+module.exports = {
+    registerValidate,
+    loginValidate
+}
