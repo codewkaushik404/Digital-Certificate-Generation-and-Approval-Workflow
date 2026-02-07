cd4663c426fdf7da36d3e6dc551ac80403a993a0
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-02-07 19:51:20 +0530

    feat(component): add signUp component to project

---
+++ Frontend/src/components/signup-step-one.jsx
@@ -0,0 +1,69 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+//import { useState } from "react"
+
+export default function SignupStepOne({ className, ...props }) {
+
+  function handleChange(e){
+    const {id, value} = e.target
+    props.setForm((prev) => ({
+      ...prev,
+      [id]: value
+    }));
+  }
+  
+  function handleSubmitOne(e){
+    e.preventDefault();
+    props.setStep((step) => step+1);
+  }
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props} onSubmit={ handleSubmitOne }>
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1 className="text-2xl font-bold">Create your account</h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            Fill in the form below to create your account
+          </p>
+        </div>
+
+        <Field>
+          <FieldLabel htmlFor="name">Name</FieldLabel>
+          <Input id="name" type="text" placeholder="Sponge Bob" value={props.form.name} onChange={handleChange} required />
+        </Field>
+
+        <Field>
+          <FieldLabel htmlFor="email">Email</FieldLabel>
+          <Input id="email" type="email" placeholder="spongebob@gmail.com" value={props.form.email} onChange={handleChange} required />
+        </Field>
+
+        <Field>
+          <FieldLabel htmlFor="password">Password</FieldLabel>
+          <Input id="password" type="password" value={props.form.password} onChange={handleChange} required />
+        </Field>
+        
+        <Field>
+          <FieldLabel htmlFor="confirm-password">Confirm Password</FieldLabel>
+          <Input id="confirm-password" type="password" required />
+          <FieldDescription>Please confirm your password.</FieldDescription>
+        </Field>
+
+        <Field>
+          <Button type="submit">Continue</Button>
+        </Field>
+        
+        <FieldDescription className="px-6 text-center">
+          Already have an account? <a href="/login">Sign in</a>
+        </FieldDescription>
+
+      </FieldGroup>
+    </form>
+  );
+}
---
+++ Frontend/src/components/signup-step-two.jsx
@@ -0,0 +1,119 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import AlertBasic from "./Alert";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export default function SignupStepTwo({className, ...props}) {
+
+  const navigate = useNavigate();
+  const [alert, setAlert] = useState({
+    type: "",
+    message: ""
+  });
+
+  function handleChange(e){
+    const {id, value} = e.target;
+    props.setForm((prev) => ({
+      ...prev,
+      [id]: value
+    }));
+  }
+
+  async function handleSubmit(e){
+    try{
+      e.preventDefault();
+      const response = await fetch("http://localhost:5000/api/v1/auth/register",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(props.form),
+      });
+      
+      const data = await response.json();
+      console.log(data);
+      if(!response.ok) setAlert({type: "error", message: data.message || "Something went wrong"});
+
+      else{
+        setAlert({type: "success", message: data.message || "Registration successful"});
+        props.setForm({
+          email: "",
+          name: "",
+          password: "",
+          instituteName: "",
+          department: "",
+          joiningYear: "",
+          duration: ""
+        });
+        setTimeout(()=>{
+          navigate("/login");
+        },2000);
+      }
+
+    }catch(err){
+      setAlert({type: "error", message: err.message || "Network Error"});
+    }
+  }
+
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}
+    onSubmit={handleSubmit} >
+      <FieldGroup>
+        {!alert.message 
+          ? (<div className="flex flex-col items-center gap-1 text-center">
+            <h1 className="text-2xl font-bold">Create your account</h1>
+            <p className="text-muted-foreground text-sm text-balance">
+              Fill in the form below to create your account
+            </p>
+          </div>)
+          : <AlertBasic alert={alert}/>
+        }
+        <Field>
+          <FieldLabel htmlFor="instituteName">Name of Institute</FieldLabel>
+          <Input id="instituteName" type="text" placeholder="Harvard University" 
+          value={props.form.instituteName} onChange={handleChange}  required/>
+        </Field>
+
+        <Field>
+          <FieldLabel htmlFor="department">Department</FieldLabel>
+          <Input id="department" type="text" placeholder="CSE" 
+          value={props.form.department} onChange={handleChange} required/>
+        </Field>
+
+        <Field>
+          <FieldLabel htmlFor="joiningYear">Year of Joining</FieldLabel>
+          <Input id="joiningYear" type="text" 
+          value={props.form.joiningYear} onChange={handleChange}  required/>
+        </Field>
+
+        <Field>
+          <FieldLabel htmlFor="duration">Duration</FieldLabel>
+          <Input id="duration" type="Number" placeholder="4" 
+          value={props.form.duration} onChange={handleChange}  required/>
+        </Field>
+
+        <Field>
+          <Button type="button" onClick={()=> props.setStep((step) => step-1)}>Back</Button>
+        </Field>
+
+        <Field>
+          <Button type="submit">Create Account</Button>
+        </Field>
+        
+        <FieldDescription className="px-6 text-center">
+          Already have an account? <a href="/login">Sign in</a>
+        </FieldDescription>
+
+      </FieldGroup>
+    </form>
+  );
+}
