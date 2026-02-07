565693d73a6df3dab86f73131c1ab70035e1d3e3
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-02-07 19:44:43 +0530

    feat: use cors middleware to allow cross origin requests

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config({quiet: true});
 const PORT = process.env.PORT || 5000
+const cors = require("cors");
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    //this is used to tell browser that it is allowed to send & receive cookies 
+    // from cross-origin-requests
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
