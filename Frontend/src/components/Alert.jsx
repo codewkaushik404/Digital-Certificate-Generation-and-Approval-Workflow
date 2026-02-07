6207df3f8b8b9d16837ec859a4ed49fa155ed86c
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-02-07 19:55:05 +0530

    feat(auth-ui): build alert component using shadcn ui

---
+++ Frontend/src/components/Alert.jsx
@@ -0,0 +1,12 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { CheckCircle2Icon, AlertCircleIcon } from "lucide-react"
+
+export default function AlertBasic({alert}) {
+
+  return (
+    <Alert variant={alert.type === "success" ? "default" : "destructive"} className="max-w-md">
+      { alert.type === "success"? <CheckCircle2Icon /> : <AlertCircleIcon /> }
+      <AlertTitle>{alert.message}</AlertTitle>
+    </Alert>
+  )
+}
