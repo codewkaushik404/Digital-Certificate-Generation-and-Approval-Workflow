131da6ea770890a4c5a429c2915e4e689e66b586
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-02-22 00:05:48 +0530

    refactor: add unique index to User model

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -4,16 +4,34 @@ const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    instituteName: {type: String, required: true, trim: true},
-    department: {type: String, enum: ["CSE", "AIML", "ISE", "CIVIL", "ECE", "EEE", "MECH"], required: true},
-    joiningYear: {type: Number, required: true, min: 1990, max: new Date().getFullYear()},    
+    password: {type: String, required: function(){ return this.strategy === "local"}},
+    googleId: {type: String, required: function(){ return this.strategy === "google"}},
+    strategy: {type: [String], enum: ["local", "google"], default: ["local"]},
+    instituteName: {type: String, trim: true},
+    department: {type: String, enum: ["CSE", "AIML", "ISE", "CIVIL", "ECE", "EEE", "MECH"]},
+    joiningYear: {type: Number, min: 1990, max: new Date().getFullYear()},    
     duration: {type: Number, default: 4, min:1, max:5},
-    status: {type: String, enum: ["active", "inactive"], default: "active"} 
+    status: {type: String, enum: ["active", "inactive"], default: "active"}, 
+    avatar: {type: String, default: "/images/userAvatar.jpg"},
+    onboardingComplete: {type: Boolean, default: false}
 }, {timestamps: true} );
+
+
+//add a unique index to only those documents in which googleId exists 
+userSchema.index(
+    {    googleId: 1}, 
+    {
+        unique: true,
+        //Normally, an index applies to all documents in a collection.
+        // With partialFilterExpression, the index only applies to documents that match a filter.
+        partialFilterExpression: { googleId: {$exists: true}}
+    }
+)
 
 //when using async function next is not used
 userSchema.pre("save", async function(){
+    if (!this.strategy.includes("local")) return;
+    
     if(!this.isModified("password")) return;
     //console.log("Inside prev-save hook");
     this.password = await bcrypt.hash(this.password, 12);
